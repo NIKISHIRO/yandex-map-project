623da51fed1aaa783d0c0f05669ce07321eade97
NIKISHIRO <alex.ruso2011@yandex.ru>
2020-05-31 22:11:33 +0400

    Updates

---
+++ precache-manifest.fcb49699e0da218d550c22119b0e5455.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5288c99068ea52481b9d10e6f471b52d",
+    "url": "/yandex-map-project/index.html"
+  },
+  {
+    "revision": "dfeb14a6eded8899ed28",
+    "url": "/yandex-map-project/static/css/main.f9fe88eb.chunk.css"
+  },
+  {
+    "revision": "e2b3f3311d40840a2476",
+    "url": "/yandex-map-project/static/js/2.da62c757.chunk.js"
+  },
+  {
+    "revision": "170ba8c50223a5b6d8f43bb1b94211a1",
+    "url": "/yandex-map-project/static/js/2.da62c757.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfeb14a6eded8899ed28",
+    "url": "/yandex-map-project/static/js/main.4eecb7ce.chunk.js"
+  },
+  {
+    "revision": "ddadc06ffa3eb0ce60cf",
+    "url": "/yandex-map-project/static/js/runtime-main.10fc0445.js"
+  }
+]);
